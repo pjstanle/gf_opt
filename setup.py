da7fd469c148cf28702f2ace77d89715383afe6a
PJ Stanley <astanley@nrel.gov>
2020-12-18 15:54:28 -0600

    setup file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 version = '0.0.1'
 
 # copy over packages
-directories = ['gf_opt']
+directories = ['src']
 
 pkg_dirs = []
 
@@ -22,32 +22,12 @@ def recursive_directories(dirs):
 
 recursive_directories(directories)
 
-# copy over package data
 
-package_data = {"tools": [str(Path("analysis") / "bos" / "BOSLookup.csv")],
-                "hybrid": []}
-
-hybrid_path = Path("hybrid")
-flicker_path = hybrid_path / "flicker" / "data"
-
-for file in glob.glob(str(flicker_path / "*shadow.txt")):
-    package_data["hybrid"].append(str(os.path.relpath(file, str(Path("hybrid")))))
-
-for file in glob.glob(str(flicker_path / "*flicker.txt")):
-    package_data["hybrid"].append(str(os.path.relpath(file, str(Path("hybrid")))))
-
-setup(name='HOPP',
+setup(name='gf_opt',
       version=version,
-      url='https://www.https://github.com/NREL/HOPP',
-      description='Hybrid Systems Optimization and Performance Platform',
-      long_description=open("RELEASE.md").read(),
-      long_description_content_type='text/markdown',
-      license='BSD 3-Clause',
-      author='NREL',
-      author_email='dguittet@nrel.gov',
+      url='https://github.com/pjstanle/gf_opt',
+      description='A collection of gradient-free optimizers',
+      author='PJ Stanley',
+      author_email='pj.stanley@nrel.gov',
       packages=pkg_dirs,
-      package_data=package_data,
-      include_package_data=True,
-      install_requires=open("requirements.txt").readlines(),
-      tests_require=['pytest']
       )
